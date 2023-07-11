a448b360823c33beb5d1ed5859299821f1c3cc46
Slava Olenin <umenokin@gmail.com>
2023-07-11 09:46:49 +0900

    feat: added clock animation

--- src/views/app.rs
+++ src/views/app.rs
@@ -6,24 +6,34 @@ use chrono_tz::{
     Europe::Paris,
 };
 
+use gloo::{
+    timers::callback::{Interval}
+};
 
 use crate::components::*;
 
 #[function_component]
 pub fn App() -> Html {
+    let time = use_state(|| Utc::now());
+    let time_state: UseStateHandle<chrono::DateTime<Utc>> = time.clone();
+    use_effect_with_deps(|_| {
+        let interval = Interval::new(1000, move || time_state.set(Utc::now()));
+        || drop(interval)
+    }, ());
+
     html! {
         <div class="board">
             <div>
-                <Clock time={Utc::now().with_timezone(&Tokyo)} />
+                <Clock time={time.with_timezone(&Tokyo)} />
                 <Plate>{"Tokyo"}</Plate>
             </div>
             <div>
-                <Clock time={Utc::now().with_timezone(&New_York)} />
+                <Clock time={time.with_timezone(&New_York)} />
                 <Plate>{"New York"}</Plate>
             </div>
 
             <div>
-                <Clock time={Utc::now().with_timezone(&Paris)} />
+                <Clock time={time.with_timezone(&Paris)} />
                 <Plate>{"Paris"}</Plate>
             </div>
         </div>
